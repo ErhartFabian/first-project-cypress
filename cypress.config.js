cb0aeb66a21fcac41a33d4a81f6d01843987dd40
Erhart Fabian Castillo Castellanos <63439046+ErhartFabian@users.noreply.github.com>
2025-07-17 00:08:18 -0600

    RE TEST

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    // Usa variables de entorno para viewportWidth y viewportHeight
+    viewportWidth: process.env.VIEWPORT_WIDTH ? Number(process.env.VIEWPORT_WIDTH) : 1900,
+    viewportHeight: process.env.VIEWPORT_HEIGHT ? Number(process.env.VIEWPORT_HEIGHT) : 1100,
   },  
 });
